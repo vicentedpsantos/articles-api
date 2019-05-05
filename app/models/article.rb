89b4b9dd8d56596fae117ebd90750a5dd6da6774
Vicente Santos <vicentedpsantos@gmail.com>
2019-05-05 11:35:15 -0300

    Added tests to the article model, tweeked with the test configurations

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ApplicationRecord
+  validates :title,
+    presence: true
+
+  validates :content,
+    presence: true
+
+  validates :slug,
+    presence: true,
+    uniqueness: true
+end
