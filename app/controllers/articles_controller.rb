2535b4ed02b0356c98ec0531ac23e771d4115812
Vicente Santos <vicentedpsantos@gmail.com>
2019-05-05 12:36:43 -0300

    Added fast_jsonapi serializer gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,15 @@
 class ArticlesController < ApplicationController
   def index
-
+    render json: serializer.new(Article.all)
   end
 
   def show
+    render json: serializer.new(Article.find(params[:id]))
+  end
 
+  private
+
+  def serializer
+    ArticleSerializer
   end
 end
